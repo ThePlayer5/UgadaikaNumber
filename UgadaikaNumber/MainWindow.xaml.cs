68faa7ac17b0b5eb89ad65e65efdd106299fd9f0
212301 <212301@1220-014-22>
2025-06-03 15:51:46 +0300

    Доделал

--- UgadaikaNumber/MainWindow.xaml.cs
+++ UgadaikaNumber/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using System.Globalization;
 
 namespace UgadaikaNumber
 {
@@ -20,20 +22,32 @@ namespace UgadaikaNumber
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Item> AttempsList { get; set; } = new ObservableCollection<Item>();
         static Random random = new Random();
         public int rndNum = random.Next(0, 100);
         public int counter = 0;
         public MainWindow()
         {
             InitializeComponent();
+            AttempsListLb.ItemsSource = AttempsList;
             MessageBox.Show($"{rndNum}");
             Bg.Background = Brushes.LightSlateGray;
             Input.IsReadOnly = false;
+            CheckNumBtn.IsEnabled = true;
+            Attemps.Text = $"Осталось дел: 0";
         }
 
         private void CheckNumBtn_Click(object sender, RoutedEventArgs e)
         {
-            counter++;
+            UpdateCounter();
+            if (!string.IsNullOrWhiteSpace(Input.Text))
+            {
+                AttempsList.Add(new Item
+                {
+                    Number = $"Число: {Input.Text}",
+                    Count = $"Попытка: {counter}"
+                });
+            }
             if (!string.IsNullOrEmpty(Input.Text))
             {
                 int userNum = int.Parse(Input.Text);
@@ -58,19 +72,34 @@ namespace UgadaikaNumber
                     Input.Background = Brushes.Gray;
                     MessageBox.Show("Поздравляем, вы угадали число за X попыток!");
                     Input.IsReadOnly = true;
+                    CheckNumBtn.IsEnabled = false;
                 }
             }
+            Input.Text = string.Empty;
         }
 
         private void ResetBtn_Click(object sender, RoutedEventArgs e)
         {
             counter = 0;
+            Attemps.Text = $"Осталось дел: {counter}";
             Input.Text = string.Empty;
             Bg.Background = Brushes.LightSlateGray;
             Input.Background = Brushes.White;
             Input.IsReadOnly = false;
+            CheckNumBtn.IsEnabled = true;
             rndNum = random.Next(0, 100);
             MessageBox.Show($"{rndNum}");
+            AttempsList.Clear();
         }
+        public void UpdateCounter()
+        {
+            counter++;
+            Attemps.Text = $"Осталось дел: {counter}";
+        }
+    }
+    public class Item
+    {
+        public string Number { get; set; }
+        public string Count { get; set; }
     }
 }
